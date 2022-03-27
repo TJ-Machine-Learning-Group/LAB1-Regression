e8a3345ba1d49c9752a9fb69bd732d44b75864e6
Saltymola <57933905+Saltymola@users.noreply.github.com>
2022-03-27 21:00:55 +0800

    Update Concrete.py

--- Concrete.py
+++ Concrete.py
@@ -36,23 +36,24 @@ def main(data_url):
     rfr_handwriting_se = myRandomForest(random_state=2, n_estimators=10, max_features=4, max_depth=12, min_change=0.001,min_samples_leaf=1, min_samples_split=2)
     rfr_handwriting_ae = myRandomForest(criterion="MAE", random_state=2, n_estimators=10, max_features=4, max_depth=12, min_change=0.001,min_samples_leaf=1, min_samples_split=2)
     
-    
     # Multi-Layer Perceptron
     mlp_skl = MLPRegressor(hidden_layer_sizes=(100,70),max_iter=1800)
     mlp_handwriting=MLPHandWrite(network_struct=(data.shape[1],9,5,1),reg_const=1)
     
-    models = [lr_skl, lasso_skl, ridge_skl, dtr_skl_mse, dtr_skl_fmse, dtr_skl_mae, dtr_handwriting, rfr_skl_se,rfr_skl_ae,rfr_skl_p,rfr_handwriting_se,rfr_handwriting_ae,mlp_skl,mlp_handwriting]
+    models = [lr_skl, lasso_skl, ridge_skl,lr_handwriting,lasso_handwriting, ridge_handwriting, dtr_skl_mse, dtr_skl_fmse, dtr_skl_mae, dtr_handwriting, rfr_skl_se,rfr_skl_ae,rfr_skl_p,rfr_handwriting_se,rfr_handwriting_ae,mlp_skl,mlp_handwriting]
     names = ["Linear Regression from sklearn", "Lasso Regression from sklearn", "Ridge Regression from sklearn", 
-        "Linear Regression writing by hand", "Lasso Regression writing by hand", "Ridge Regression writing by hand", 
-         "Decision Tree Regressor from sklearn(squared_error)", "Decision Tree Regressor from sklearn(friedman_mse)",
-         "Decision Tree Regressor from sklearn(absolute_error)","Decision Tree Regressor writing by hand", 
-            "Random Forest Regressor from sklearn(squared_error)","Random Forest Regressor from sklearn(absolute_error)","Random Forest Regressor from sklearn(poisson)","Random Forest Regressor written by hand(squared_error)","Random Forest Regressor written by hand(absolute_error)","Multi-Layer Perceptron Regressor from sklearn",
+            "Linear Regression writing by hand", "Lasso Regression writing by hand", "Ridge Regression writing by hand", 
+            "Decision Tree Regressor from sklearn(squared_error)", "Decision Tree Regressor from sklearn(friedman_mse)",
+            "Decision Tree Regressor from sklearn(absolute_error)","Decision Tree Regressor writing by hand", 
+            "Random Forest Regressor from sklearn(squared_error)","Random Forest Regressor from sklearn(absolute_error)",
+            "Random Forest Regressor from sklearn(poisson)","Random Forest Regressor written by hand(squared_error)",
+            "Random Forest Regressor written by hand(absolute_error)","Multi-Layer Perceptron Regressor from sklearn",
             "Multi-Layer Perceptron writing by hand"]
     
     for i in range(len(models)):
         #参数为5折验证，测试集占20%
         print(names[i])
-        Regression(models[i],data,target,splits=1,size=0.2,model_name=names[i])
+        Regression(models[i],data,target,splits=5,size=0.2,model_name=names[i])
 
 if __name__=='__main__':
     url="./Concrete_Data.xls"
